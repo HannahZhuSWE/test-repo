ae7e56299349477c82b1242804ac8d692aa75b58
Hannah Zhu <t-hannzhu@microsoft.com>
2022-06-10 16:03:38 -0700

    added a more complex test

--- tests/temp-tests.spec.ts
+++ tests/temp-tests.spec.ts
@@ -4,3 +4,16 @@ test('website loaded', async ({page}) =>{
     await page.goto('/');
     await expect(page.locator('a')).toContainText('Learn React')
 })
+
+test('links to react home page', async ({page}) =>{
+    await page.goto('/');
+    
+    const [popup] = await Promise.all([
+        // It is important to call waitForEvent before click to set up waiting.
+        page.waitForEvent('popup'),
+        // Opens popup.
+        page.click('text=Learn React')
+      ]);
+    await popup.waitForLoadState();
+    await expect (popup).toHaveURL('https://reactjs.org/')
+})
