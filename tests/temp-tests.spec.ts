63a9317ca172fb39b76a2827831fd2a0b1101b88
Hannah Zhu <t-hannzhu@microsoft.com>
2022-06-10 14:43:01 -0700

    first commit

---
+++ tests/temp-tests.spec.ts
@@ -0,0 +1,6 @@
+import {test, expect} from '@playwright/test';
+
+test('website loaded', async ({page}) =>{
+    await page.goto('/');
+    await expect(page.locator('a')).toContainText('Learn React')
+})
